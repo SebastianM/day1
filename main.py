fe962df4650f731f5e613302b926810f5c68b69f
Sebastian Matthews <109438074+SebastianM@users.noreply.github.com>
2022-07-21 20:09:59 +0000

    Madlib

---
+++ main.py
@@ -0,0 +1,20 @@
+name = input("what is your name: ")
+food = input("enter a food: ")
+drink = input("enter a drink: ")
+noun = input("enter a noun: ")
+noun2 = input("enter a noun: ")
+verb = input("enter a verb: ")
+verb2 = input("enter a verb: ")
+verb3 = input("enter a verb: ")
+adj = input("enter a adjective: ")
+
+print(" ")
+print(f"your name is {name}, and today is your favourite day of the school year. That's because they are serving {food} and {drink} at lunch.in class, you can't stop {verb} about {food} and {verb2} all over your {noun}. you {verb3} to your {noun2} to eat.today is deffinetly the {adj} day ever!")
+
+
+
+
+
+
+
+#revolution,enviorment,circulating,detecting,perceiving,distopian
